1e10b538072976185450862101ee4b86bb890066
Jude-TN <Jude@transactionnerd.com>
2026-07-16 19:28:31 -0400

    Delete transactionnerd-app/transactionnerd-app 3 directory

--- transactionnerd-app/transactionnerd-app 3/components/ProgressBar.tsx
+++
@@ -1,7 +0,0 @@
-export default function ProgressBar({ percent, color = "bg-teal" }: { percent: number; color?: string }) {
-  return (
-    <div className="bg-line rounded-sm h-1.5 overflow-hidden">
-      <div className={`${color} h-full rounded-sm transition-all`} style={{ width: `${percent}%` }} />
-    </div>
-  );
-}
